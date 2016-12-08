9e7a0f8d3e3afde1fd40dca52b8408a41a825d70
Chrisjahim <chrisjahim@free.fr>
2016-12-08 11:09:10 +0100

    Ajout de archiviste.c basique

---
+++ archiviste.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+/*********************************/
+/*        Coding Party           */
+/*      8/9 décembre 2016        */
+/*      JEAMME Christopher       */
+/*       BRUYERE Dimitri         */
+/*********************************/
+
+int main(int argc, char* argv[])
+{
+    /*Vérification du nombre d'arguments*/
+        if (argc != 3)
+        {
+            fprintf(stderr, "Erreur de nombre d'argument pour archiviste.c");
+            exit(-1);
+        }
+
+
+    return 0;
+}
